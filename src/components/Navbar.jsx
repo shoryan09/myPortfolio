a46b9b2afba48ef58836ec83a62b395c2324b5b2
shoryan09 <anshoryr1200@gmail.com>
2026-05-05 20:30:42 +0530

    resume updates

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -57,7 +57,7 @@ export default function Navbar() {
 
         {/* HUD Action */}
         <div className="hud-nav__actions">
-          <a href="https://drive.google.com/file/d/1QAhDrRq2XPwIL-9U6SYgX8Ug8XjTWYFl/view" target="_blank" rel="noopener noreferrer" className="hud-btn">
+          <a href="https://drive.google.com/file/d/14mu4vWJrloPCUwjh6K2XjMfE4t31mtsd/view" target="_blank" rel="noopener noreferrer" className="hud-btn">
             [ ARCHIVE_RESUME ] ↗
           </a>
         </div>
