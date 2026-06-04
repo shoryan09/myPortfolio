e3c3707f7c6e5bc10d407b2b225b868dcaa72601
shoryan09 <anshoryr1200@gmail.com>
2026-06-04 09:45:40 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 import { initLenis } from './lenis';
+import { Analytics } from "@vercel/analytics/react"
 import './index.css';
 
 export default function App() {
